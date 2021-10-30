70c743d17ef9d3a9b1253dcb2f22d61339c2cc4d
juand <domingoortin.j@gmail.com>
2021-10-31 00:33:35 +0200

    Added splash screen

--- app/src/main/java/com/juandomingo/mypharmamemory/MainActivity.kt
+++ app/src/main/java/com/juandomingo/mypharmamemory/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        Thread.sleep(3000)
+        setTheme(R.style.Theme_MyPharmaMemory2)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
